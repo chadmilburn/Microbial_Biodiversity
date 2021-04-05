23e95952a5091a3d767602d0211d2da728902f81
Chad Milburn <cmilburn@ballstate.bsu.edu>
2021-04-04 23:19:19 -0400

    looking back  over week 15 and working th problem

---
+++ static/js/app.js
@@ -0,0 +1,11 @@
+// open python server
+// python -m http.server
+
+// 
+d3.json("samples.json").then((sampleData)=>{
+    // console.log(sampleData)
+    console.log(sampleData)
+    // var subjectIDS = sampleData.names;
+    // console.log(subjectIDS)
+})
+
